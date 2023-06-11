015dfc7f738d6e33a4807ce0e1e68c03a3c92a71
klownie <audrickyeu@proton.me>
2023-06-11 18:51:02 +0700

    works

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,5 @@
-#![allow(dead_code)]
-
-use bevy::prelude::*;
-use bevy::window::PresentMode;
-use bevy::window::PrimaryWindow;
-
-/* use bevy::window::WindowTheme; */
-use bevy_prototype_lyon::prelude::*;
+use burtle::*;
 use std::collections::HashMap;
-
-const ALPHABET: [char; 2] = ['F', 'X'];
-const S: [char; 4] = ['+', '-', '[', ']'];
-const Þ: &str = "F";
-const P: [&'static str; 2] = ["F -> F+F-F-F+F+", "X -> F+X+F"];
-const DERIVATION: usize = 4;
-const ANGLE: f32 = 90.0;
-const ENTITY_SPEED: f32 = 5.5;
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "FLOAT".into(),
-                resolution: (1920., 1080.).into(),
-                present_mode: PresentMode::AutoVsync,
-                // Tells wasm to resize the window according to the available canvas
-                fit_canvas_to_parent: false,
-                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
-                prevent_default_event_handling: false,
-                /*  window_theme: Some(WindowTheme::Dark), */
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_plugin(ShapePlugin)
-        .add_startup_systems((spawn_camera, spawn_turtle))
-        .add_system(turtle_mouvement)
-        .run();
-}
-
-#[derive(Component)]
-pub struct Turtle {
-    horizontale: f32,
-    crayon: bool,
-    pos_enregistre: (f32, Vec3),
-    instruction: String,
-}
-
-impl Turtle {
-    pub fn right(&mut self, angle: f32) {
-        self.horizontale += angle
-    }
-    pub fn left(&mut self, angle: f32) {
-        self.horizontale -= angle
-    }
-}
-
-pub fn spawn_turtle(
-    mut commands: Commands,
-    window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>,
-) {
-    let window = window_query.get_single().unwrap();
-    let mut þ = Þ.to_owned();
-    derive_iter(&mut þ, DERIVATION, &P);
-    commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                flip_x: false,
-                flip_y: false,
-                custom_size: Some(Vec2::new(100.0, 100.0)),
-                anchor: Default::default(),
-                ..default()
-            },
-            transform: Transform::from_xyz(window.width() / 3.0, window.height() / 2.0, 0.0),
-            /*             transform: Transform::from_xyz(0.0, window.height() , 0.0), */
-            texture: asset_server.load("sprites/turtle.png"),
-            ..default()
-        },
-        Turtle {
-            horizontale: 0.0,
-            crayon: true,
-            pos_enregistre: (
-                0.0,
-                Vec3::new(window.width() / 2.0, window.height() / 2.0, 0.0),
-            ),
-            instruction: þ,
-        },
-    ));
-}
-
-pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
-    let window = window_query.get_single().unwrap();
-
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-        ..default()
-    });
-}
 
 fn derive_iter<'a>(þ: &'a mut String, n: usize, p: &[&'static str; 2]) {
     let mut rules: HashMap<char, String> = HashMap::new();
@@ -122,62 +25,29 @@ fn derive_iter<'a>(þ: &'a mut String, n: usize, p: &[&'static str; 2]) {
     }
 }
 
-fn dessiner(mut turtle: &mut Turtle, angle: f32, transform: &mut Transform) {
-    let mut remove_chars = 0;
-
-    for character in turtle.instruction.chars() {
-        remove_chars += 1;
+fn dessiner(burtle: &mut Burtle, chaine: &String, angle: f32, length: f32) {
+    for character in chaine.chars() {
         match character {
-            '+' => turtle.horizontale += angle,
+            '+' => burtle.right(angle),
 
-            '-' => turtle.horizontale -= angle,
+            '-' => burtle.left(angle),
 
-            '[' => {
-                turtle.pos_enregistre = (turtle.horizontale.clone(), transform.translation.clone())
-            }
+            '[' => {}
 
-            ']' => (turtle.horizontale, transform.translation) = turtle.pos_enregistre,
+            ']' => {}
 
-            _ => break,
+            _ => burtle.forward(length),
         }
     }
-
-    turtle.instruction.replace_range(..remove_chars, "");
 }
 
-pub fn turtle_mouvement(
-    mut commands: Commands,
-    mut turtle_query: Query<(&mut Transform, &mut Turtle)>,
-) {
-    for (mut transform, mut turtle) in turtle_query.iter_mut() {
-        let direction;
+fn main() {
+    let mut þ = String::from("X");
+    let p = ["F -> X+F+X", "X -> F-X-F"];
+    derive_iter(&mut þ, 8, &p);
 
-        dessiner(&mut turtle, ANGLE, transform.as_mut());
-
-        direction = Vec3::new(
-            turtle.horizontale.to_radians().cos(),
-            turtle.horizontale.to_radians().sin(),
-            0.0,
-        );
-
-        println!("chaine = {}", &turtle.instruction);
-
-        let old_position = transform.translation.clone();
-        transform.translation += direction * ENTITY_SPEED;
-
-        // Spawn a line segment entity
-
-        let shape = shapes::Line(
-            Vec2::new(old_position.x, old_position.y),
-            Vec2::new(transform.translation.x, transform.translation.y),
-        );
-
-        commands.spawn((
-            ShapeBundle {
-                path: GeometryBuilder::build_as(&shape),
-                ..default()
-            },
-            Stroke::new(Color::BLACK, 2.0),
-        ));
-    }
+    let mut burtle = Burtle::new();
+    burtle.pen_down();
+    dessiner(&mut burtle, &þ, 60., 3.);
+    burtle.setup(1000., 1000.)
 }
